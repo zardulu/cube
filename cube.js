4c636a5cca4947cc42a7b8e6c10d00f0e3045bc7
ansh <anshulgiri000@gmail.com>
2023-04-08 23:03:54 +0530

    update wireframe

--- cube.js
+++ cube.js
@@ -7,8 +7,13 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
+const material = new THREE.LineBasicMaterial( {
+	color: 0xffffff,
+	
+} );
+const cube = new THREE.LineSegments( new THREE.EdgesGeometry(geometry),
+ material );
+
 scene.add( cube );
 
 camera.position.z = 3;
